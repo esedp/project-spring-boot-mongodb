f4207fe81a8340728a7eed796c092e2c4708118d
José Luiz <jlnalbertii@gmail.com>
2024-08-26 02:42:34 -0300

    connection to mongodb with UserRepository and UserService

--- src/main/java/org/example/projectmongo/resources/UserResource.java
+++ src/main/java/org/example/projectmongo/resources/UserResource.java
@@ -1,26 +1,26 @@
 package org.example.projectmongo.resources;
 
 import org.example.projectmongo.domain.User;
+import org.example.projectmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria Brown", "maria@gmail.com");
-        User alex = new User("2", "Alex Green", "alex@gmail.com");
-        List<User> list = new ArrayList<>();
 
-        list.addAll(Arrays.asList(maria, alex));
+        List<User> list = service.findAll();
 
         return ResponseEntity.ok().body(list);
     }
---
+++ src/main/java/org/example/projectmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package org.example.projectmongo.services;
+
+import org.example.projectmongo.domain.User;
+import org.example.projectmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
