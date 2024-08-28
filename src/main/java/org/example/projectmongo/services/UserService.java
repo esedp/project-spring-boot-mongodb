b05e3d0fd884e8932269b873fdeebe3dd0614257
José Luiz <jlnalbertii@gmail.com>
2024-08-28 00:27:30 -0300

    user insertion with POST

--- src/main/java/org/example/projectmongo/services/UserService.java
+++ src/main/java/org/example/projectmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package org.example.projectmongo.services;
 
 import org.example.projectmongo.domain.User;
+import org.example.projectmongo.dto.UserDTO;
 import org.example.projectmongo.repository.UserRepository;
 import org.example.projectmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
