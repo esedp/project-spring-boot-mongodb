51cea8e89a8e4b86b7ad615c00846f164a452f5e
José Luiz <jlnalbertii@gmail.com>
2024-08-28 02:36:31 -0300

    getting a post by id

---
+++ src/main/java/org/example/projectmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package org.example.projectmongo.services;
+
+import org.example.projectmongo.domain.Post;
+import org.example.projectmongo.repository.PostRepository;
+import org.example.projectmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = postRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+
+    }
+}
